5b7349d2c301a8c07eacebaf488136d1f6add8a9
Aditya Upadhyay <120381566+Uaditya18@users.noreply.github.com>
2025-03-10 13:05:03 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://healthy-fy-backend-production.up.railway.app/api/v1";
+export const BASE_URL = "https://healthyfy-backend.onrender.com";
 export const token = localStorage.getItem("token")
